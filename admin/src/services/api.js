705d9f1d6db86441215666bb7f273d3c2c11eb64
bso26king-ops <bso26king@gmail.com>
2026-04-16 17:33:37 -0500

    fix: unwrap response.data in axios interceptor for all API calls

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -19,7 +19,7 @@ api.interceptors.request.use(
 
 // Response interceptor for 401
 api.interceptors.response.use(
-  (response) => response,
+  (response) => response.data,
   (error) => {
     if (error.response?.status === 401) {
       localStorage.removeItem('adminToken');
